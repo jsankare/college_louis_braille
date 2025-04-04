3257692cc6f77099c60ede5dac28f9c18a1ecfd7
Brubru <jordan.sankare@gmail.com>
2025-04-04 20:33:08 +0200

    init

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div>
+            <h1>À propos</h1>
+            <p>Bienvenue sur la page à propos.</p>
+        </div>
+    );
+}
---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>© 2025 College Louis Braille - Tous droits réservés</p>
+        </footer>
+    );
+}
